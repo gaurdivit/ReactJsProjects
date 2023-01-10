f35e0d62a12251d25a68668ffd80fe271b727d00
divit gaur <divitgaur@divits-MacBook-Pro.local>
2023-01-10 14:30:53 +0530

    using System of react

---
+++ animalShow/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimals() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimals()]);
+  };
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}></AnimalShow>;
+  });
+
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderAnimals}</div>
+    </div>
+  );
+}
+
+export default App;
